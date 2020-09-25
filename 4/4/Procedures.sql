a5c8123e20db58f2e0601e69d1b63500d722a49d
Vya4eslav111 <vya4eslav.seleznev@gmail.com>
2020-09-25 13:41:30 +0300

    4.4 - 90%

--- 4/4/Procedures.sql
+++ 4/4/Procedures.sql
@@ -21,39 +21,37 @@ DECLARE @kol int;
 SET @kol=1;
 EXEC Count_Students_Sem @kol;
 
-/*3.1_Не выводит*/
-CREATE PROCEDURE List_Students_Dir (@Dir AS INT, @Disc AS VARCHAR(30))
+/*3.1*/
+CREATE PROCEDURE List_Students_Dir(@Dir AS INT, @Disc AS VARCHAR(30))
 AS
 SELECT Distinct Students.FIO
-FROM Groups
-JOIN Students ON Groups.IdGroup=Students.GroupId
-JOIN Balls ON Students.IdSt=Balls.NumSt JOIN Uplans ON Uplans.IdDisc=Balls.IdDisc
+FROM Students
+JOIN Groups ON Students.GroupId = Groups.IdGroup
+JOIN Balls ON Students.IdSt=Balls.NumSt
+JOIN Uplans ON Uplans.IdDisc=Balls.IdDisc
 WHERE Groups.NumDir=@Dir and NumDisc =
 (
 	SELECT NumDisc
 	FROM Disciplines
-	WHERE Name=@Disc
+	WHERE Name = @Disc
 );
 GO
 
-EXEC List_Students_Dir 230100, 'Физика'
+EXEC List_Students_Dir 1, 'Физика'
 
 DECLARE @dir int, @title varchar(30);
-SET @dir=230100;
+SET @dir = 1;
 SET @title ='Физика';
-EXEC List_Students_Dir @dir,@title;
+EXEC List_Students_Dir @dir, @title;
 
 /*3.2*/
-CREATE PROCEDURE Enter_Students
-(	
-	@Fio AS VARCHAR(30), @Group AS VARCHAR(10)
-) 
+CREATE PROCEDURE Enter_Students(@Fio AS VARCHAR(30), @Group AS VARCHAR(10)) 
 AS
 INSERT INTO Students (FIO, GroupId) VALUES (@Fio, @Group);
 GO
 
 DECLARE @Stud VARCHAR(30), @Group int;
-SET @Stud='Светлова Вероника';
+SET @Stud='Петров Петр';
 SET @Group = 1;
 EXEC Enter_Students  @Stud, @Group;
 
@@ -64,9 +62,8 @@ UPDATE Groups SET NumGr=CONVERT(char(1),CONVERT(int, LEFT(NumGr,1))+1)+ SUBSTRIN
  WHERE NumGr=@Group;
 GO
 
-
 DECLARE @Group VARCHAR(10);
-SET @Group='13504/3';
+SET @Group='13504/4';
 EXEC Next_Course @Group;
 GO
 
@@ -84,9 +81,6 @@ EXEC Number_Groups 2, @Group OUTPUT;
 SELECT @Group;
 GO
 
-EXEC Next_Course_2;
-GO
-
 /*6*/
 CREATE PROCEDURE Delete_Students_Complete
 AS
@@ -99,8 +93,12 @@ GO
 CREATE PROCEDURE Next_Course_2 
 AS
 EXEC Delete_Students_Complete;
-UPDATE Students SET GroupId=CONVERT(GroupId + 1)
-WHERE GroupId IN (SELECT GroupId FROM Students_complete_2);
+UPDATE Groups SET NumGr=CONVERT(char(1),CONVERT(int, LEFT(NumGr,1))+1)+ SUBSTRING(NumGr,2,LEN(NumGr)-1)
+WHERE IdGroup IN
+(
+	SELECT IdGroup
+	FROM Students_complete_2
+);
 GO
 
 EXEC Next_Course_2;
